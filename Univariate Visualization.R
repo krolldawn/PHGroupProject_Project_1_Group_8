dfd0b9094725601bb318f5010cf344ad1ae85a8e
abrilzar25 <abrilzarate@csu.fullerton.edu>
2026-03-08 13:53:49 -0700

    Update Univariate Visualization.R
    Added color, fixed functions

--- Univariate Visualization.R
+++ Univariate Visualization.R
@@ -24,7 +24,9 @@ hist(as.numeric(HINTSData_2020_clean$V5), #Extracted Age variable (V5) to create
 # 4) Boxplot for BMI
 boxplot(as.numeric(HINTSData_2020_clean$V14), #Extraced BMI (V14) to create boxplot where BMI converted to numeric variable
         main = "Distribution of BMI", #Added "Distribution of BMI" as title of boxplot
-        ylab = "BMI") #Assigned BMI to y-axis
+        ylab = "BMI", #Assigned BMI to y-axis
+        col = "purple4", #Added purple color to the boxplot
+        border = "black") #Added black outline to the boxplot
 
 # Part III
 library(ggplot2) #Downloaded ggplot2 package
@@ -47,13 +49,13 @@ ggplot(df<-HINTSData_2020_clean, aes(x = V12)) + #Assigned RaceEthnicity (V12) t
 df <- HINTSData_2020_clean
 
 ggplot(df, aes(x = as.numeric(V5))) + #Created ggplot for Age, where age variable converted to numeric and assigned to x-axis
-  geom_histogram(bins = 20) + #Created a histogram with 20 bins
+  geom_histogram(bins = 20, fill = "purple4", color = "black") + #Created a histogram with 20 bins and added purple color to the bars with black outlines
   labs(title = "Age Distribution", x = "Age", y = "Count") + #Added "Age Distribution" title to chart, "Age" to x-axis and "Count" to y-axis
   theme_minimal() #Added minimal theme to chart
 
 # BMI (boxplot)
 ggplot(df, aes(y = as.numeric(V14))) + #Created ggplot for BMI, where BMI variable converted to numeric and assigned to y-axis
-  geom_boxplot() + #Created boxplot
+  geom_boxplot(fill = "purple4", color = "black") + #Created boxplot and added purple color with black outline
   labs(title = "BMI Distribution", y = "BMI") + #Added "BMI Distribution" as title of boxplot and "BMI" to y-axis
   theme_minimal() #Added minimal theme to chart
 
@@ -62,14 +64,16 @@ ggplot(df, aes(y = as.numeric(V14))) + #Created ggplot for BMI, where BMI variab
 my_visual_func_I <- function(data) { #Created new variable my_visual_func_I for dataset
   
   # Barplot for QualityCare
-  barplot(table(HINTSData_2020_clean$V3), #Created barplot for distribution of Quality Care
-          main = "Distribution of QualityCare", #Added "Distribution of QualityCare" as title of barplot
-          xlab = "QualityCare", #Assigned Quality Care to x-axis
-          ylab = "Frequency") #Assigned Frequency to y-axis
+barplot(table(HINTSData_2020_clean$V3), #extracted the V3 (Quality Care) variable from the dataset to create barplot
+        main = "Distribution of QualityCare", #Added "Distribution of Quality Care" title to the bar plot
+        xlab = "QualityCare", #Assigned Quality Care to x-axis
+        ylab = "Frequency", #Assigned Frequency to y-axis
+        col = "purple4") #Assigned color to bars
   
   # Pie chart for RaceEthnicity
-  pie(table(HINTSData_2020_clean$V12), #Created piechart for Race/Ethnicity
-      main = "Distribution of RaceEthnicity") #Added title "Distribution of RaceEthn5" to piechart
+pie(table(HINTSData_2020_clean$V12), #Extracted RaceEthn5 variable to create pie chart
+        main = "Distribution of RaceEthn5", #Added "Distribution of RaceEthn5" as title of the pie chart
+    col = c("steelblue", "purple3", "red3", "seagreen", "orange1", "skyblue")) #Assigned colors to pie chart sections
 }
 my_visual_func_I(HINTSData_2020_clean) 
 
@@ -80,11 +84,13 @@ my_visual_func_II <- function(HINTSData_2020_clean) { #Created new variable my_v
   # Histogram for Age
   hist(as.numeric(HINTSData_2020_clean$V5), #Created histogram for Age, where age converted to numeric variable
        main = "Distribution of Age", #Added "Distribution of Age" as title of histogram
-       xlab = "Age") #Assigned Age to x-axis
-  
+       xlab = "Age" #Assigned Age to x-axis
+       col= "green3") #Assigned color to bars in histogram
   # Boxplot for BMI
-  boxplot(as.numeric(HINTSData_2020_clean$V14), #Created boxplot for BMI, where BMI converted to numeric variable
-          main = "Distribution of BMI", #Added "Distribution of BMI" as title for boxplot
-          ylab = "BMI") #Assigned BMI to y-axis
+boxplot(as.numeric(HINTSData_2020_clean$V14), #Extraced BMI (V14) to create boxplot where BMI converted to numeric variable
+        main = "Distribution of BMI", #Added "Distribution of BMI" as title of boxplot
+        ylab = "BMI", #Assigned BMI to y-axis
+        col = "purple4", #Added purple color to the boxplot
+        border = "black") #Added black outline to the boxplot
 }
 my_visual_func_II(HINTSData_2020_clean) #Produced the Age histogram and BMI boxplot
