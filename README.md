eb50b6c737679ae5429d26a5f4ecdd3bbe873cf5
Kevin Roldan <kevinroldan13@csu.fullerton.edu>
2026-03-08 12:31:37 -0700

    #Added interpreation to univariate visualization and added to main analysis

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # PHGroupProject_Project_1_Group_8
 PUBH 422 Group Project 1 Group 8
-Kylize
-Kevin 
 Abril
+Kevin
+Kylize
+
--- Univariate Visualization.R
+++ Univariate Visualization.R
@@ -1,57 +1,57 @@
 # Day 4 Univariate Visualization
-table(HINTSData_2020_clean)
+table(HINTSData_2020_clean) #Created frequency table for HINTSData table
 
 # Part I. 
 # 1) Bar plot for QualityCare
-barplot(table(HINTSData_2020_clean$V3),
-        main = "Distribution of QualityCare",
-        xlab = "QualityCare",
-        ylab = "Frequency",
-        col = "purple4")
+barplot(table(HINTSData_2020_clean$V3), #extracted the V3 (Quality Care) variable from the dataset to create barplot
+        main = "Distribution of QualityCare", #Added "Distribution of Quality Care" title to the bar plot
+        xlab = "QualityCare", #Assigned Quality Care to x-axis
+        ylab = "Frequency", #Assigned Frequency to y-axis
+        col = "purple4") #Assigned color to bars
       
 # 2) Pie chart for RaceEthn5
-pie(table(HINTSData_2020_clean$V12),
-        main = "Distribution of RaceEthn5",
-    col = c("steelblue", "purple3", "red3", "seagreen", "orange1", "skyblue"))
+pie(table(HINTSData_2020_clean$V12), #Extracted RaceEthn5 variable to create pie chart
+        main = "Distribution of RaceEthn5", #Added "Distribution of RaceEthn5" as title of the pie chart
+    col = c("steelblue", "purple3", "red3", "seagreen", "orange1", "skyblue")) #Assigned colors to pie chart sections
 
 # Part II.         
 # 3) Histogram for Age
-hist(HINTSData_2020_clean$V5,
-     main = "Distribution of Age",
-     xlab = "Age")
+hist(HINTSData_2020_clean$V5, #Extracted Age variable (V5) to create histogram
+     main = "Distribution of Age", #Added "Distribution of Age" as title of historgram
+     xlab = "Age") #Assigned Age to x-axis
 
 # 4) Boxplot for BMI
-boxplot(HINTSData_2020_clean$V14,
-        main = "Distribution of BMI",
-        ylab = "BMI")
+boxplot(HINTSData_2020_clean$V14, #Extraced BMI (V14) to create boxplot
+        main = "Distribution of BMI", #Added "Distribution of BMI" as title of boxplot
+        ylab = "BMI") #Assigned BMI to y-axis
 
 # Part III
-library(ggplot2)
-df<-HINTSData_2020_clean
+library(ggplot2) #Downloaded ggplot2 package
+df<-HINTSData_2020_clean #Assigned df name to dataset
 
 # QualityCare (bar)
-ggplot(df<-HINTSData_2020_clean, aes(x = V3)) +
-  geom_bar() +
-  labs(title = "QualityCare Distribution", x = "QualityCare", y = "Count") +
-  theme_minimal()
+ggplot(df<-HINTSData_2020_clean, aes(x = V3)) + #Assigned QualityCare (V3) to x-axis of ggplot
+  # geom_bar() + #Created barchart
+  labs(title = "QualityCare Distribution", x = "QualityCare", y = "Count") + #Added "QualityCare Distribution" title to chart, "QualityCare" to x-axis and "Count" to y-axis
+  theme_minimal() #Added minimal theme to chart
 
 # RaceEthn5 (pie)
-ggplot(df<-HINTSData_2020_clean, aes(x = V12)) +
-  geom_bar() +
-  coord_flip() +
-  labs(title = "RaceEthn5 Distribution", x = "RaceEthn5", y = "Count") +
-  theme_minimal()
+ggplot(df<-HINTSData_2020_clean, aes(x = V12)) + #Assigned RaceEthn5 (V12) to x-axis of ggplot
+  geom_bar() + #Created barchart
+  coord_flip() + #Flipped axis for vertical bars
+  labs(title = "RaceEthn5 Distribution", x = "RaceEthn5", y = "Count") + #Added "RaceEthn5 Distribution" title to chart, "RaceEthn5" to x-axis and "Count" to y-axis
+  theme_minimal() #Added minimal theme to chart
 
 # Age (histogram)
 df <- HINTSData_2020_clean
 
-ggplot(df, aes(x = as.numeric(V5))) +
-  geom_histogram(bins = 20) +
-  labs(title = "Age Distribution", x = "Age", y = "Count") +
+ggplot(df, aes(x = as.numeric(V5))) + #Created ggplot for Age, where age converted to numeric
+  geom_histogram(bins = 20) + #Created a histogram with 20 bins
+  labs(title = "Age Distribution", x = "Age", y = "Count") + #Added "Age Distribution" title to chart, "Age" to x-axis and "Count" to y-axis
   theme_minimal()
 
 # BMI (boxplot)
-ggplot(df, aes(y = as.numeric(V14))) +
+ggplot(df, aes(y = as.numeric(V14))) + 
   geom_boxplot() +
   labs(title = "BMI Distribution", y = "BMI") +
   theme_minimal()
